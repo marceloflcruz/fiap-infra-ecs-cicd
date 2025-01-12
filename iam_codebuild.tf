e22a378bd1ed91f35bd6de889a584c43e6fe4c02
marceloflcruz <marceloflcruz@outlook.com>
2025-01-12 13:30:25 -0300

    codebuild_role

--- iam_codebuild.tf
+++ iam_codebuild.tf
@@ -1,21 +1,41 @@
 # ----------------------------------------------------------------
 # iam_codebuild.tf
 # ----------------------------------------------------------------
-data "aws_iam_policy_document" "codebuild_assume_role" {
-  statement {
-    effect = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["*"]
-    }
-    actions = ["*"]
-  }
-}
+# data "aws_iam_policy_document" "codebuild_assume_role" {
+#   statement {
+#     effect = "Allow"
+#     principals {
+#       type        = "Service"
+#       identifiers = ["*"]
+#     }
+#     actions = ["*"]
+#   }
+# }
+
+# resource "aws_iam_role" "codebuild_role" {
+#   name               = "CodeBuildServiceRole"
+#   assume_role_policy = data.aws_iam_policy_document.codebuild_assume_role.json
+# }
 
 resource "aws_iam_role" "codebuild_role" {
-  name               = "CodeBuildServiceRole"
-  assume_role_policy = data.aws_iam_policy_document.codebuild_assume_role.json
+  name = "CodeBuildServiceRole"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
 }
+EOF
+}
+
 
 # Attach policies that allow CodeBuild to do what it needs:
 # - Pull code from CodePipeline (S3)
