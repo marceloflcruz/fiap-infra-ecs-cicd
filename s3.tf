49fe38494b75cc1d8d86aa6444871af82e7bbfdf
marceloflcruz <marceloflcruz@outlook.com>
2025-01-12 00:13:22 -0300

    codepipeline_artifacts

--- s3.tf
+++ s3.tf
@@ -2,6 +2,11 @@
 # s3.tf
 # ----------------------------------------------------------------
 resource "aws_s3_bucket" "codepipeline_artifacts" {
-  bucket = "my-terraform-codepipeline-artifacts-12345"  # Must be globally unique
+  bucket = "my-terraform-codepipeline-artifacts-${random_string.unique.result}"  # Must be globally unique
   acl    = "private"
+}
+
+resource "random_string" "unique" {
+  length  = 6
+  special = false
 }
